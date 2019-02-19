71a34ba83a33635c8a2d5d800f6c9b87bdaa6a8f
Kim Burge Strand <kim@burgestrand.se>
2019-02-19 13:20:51 +0100

    Rid ourselves of the storyboard

--- ios-app/Meetup/AppDelegate.swift
+++ ios-app/Meetup/AppDelegate.swift
@@ -9,7 +9,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let window = UIWindow()
+        self.window = window
+
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+
         return true
     }
 
