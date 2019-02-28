1302c8542d2abcd18bf981bc4dc488cd1f6afb95
Kim Burge Strand <kim@burgestrand.se>
2019-02-28 12:45:43 +0100

    Add current user to context

--- server.rb
+++ server.rb
@@ -18,10 +18,17 @@ post "/graphql" do
   current_user_id = json.dig("variables", "$current_user")
   current_user = Models::User.where(id: current_user_id).first if current_user_id
 
-  result = Schema.execute(json["query"], root_value: App.new(
-    db: $db,
-    current_user: current_user
-  ))
+  result = Schema.execute(
+    json["query"],
+    variables: json["variables"],
+    context: {
+      current_user: current_user
+    },
+    root_value: App.new(
+      db: $db,
+      current_user: current_user
+    )
+  )
 
   json result.to_h
 end
